01f02060e8c70d25b90c2865acd3947a7299ec8b
Ajmal2025 <139298518+Ajmal2025@users.noreply.github.com>
2023-10-25 22:24:54 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,11 @@
+
+
+// 36.	T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+
+
+let T_shirt= function (size:number,message:string):string{
+    return `${message} size no ${size}`
+}
+
+// function call 
+console.log(T_shirt(20,"JUST DO  IT, Body shirt"))
